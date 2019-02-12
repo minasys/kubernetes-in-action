d8ea65c8212f73d457d112db2f05a1d96ca720fd
MG <minasys@gmail.com>
2019-02-12 12:26:26 -0600

    Update minikube-with-rbac-and-psp-enabled.sh

--- Chapter13/minikube-with-rbac-and-psp-enabled.sh
+++ Chapter13/minikube-with-rbac-and-psp-enabled.sh
@@ -2,8 +2,20 @@
 
 # Enabling RBAC and PodSecurityPolicy admission control in Minikube
 
+#I’m using Minikube version v0.19.0 to run these examples. That version doesn’t
+#enable either the PodSecurityPolicy admission control plugin or RBAC authorization,
+#which is required in part of the exercises. One exercise also requires authenticating
+#as a different user, so you’ll also need to enable the basic authentication plugin
+#where users are defined in a file.
+#To run Minikube with all these plugins enabled, you may need to use this (or a similar)
+#command, depending on the version you’re using:
+
 # The following command works with minikube 0.19.0 and 0.19.1, but may not work with later versions (see command below)
 minikube start --extra-config apiserver.Authentication.PasswordFile.BasicAuthFile=/etc/kubernetes/passwd --extra-config=apiserver.Authorization.Mode=RBAC --extra-config=apiserver.GenericServerRunOptions.AdmissionControl=NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,PodSecurityPolicy,ResourceQuota,DefaultTolerationSeconds
+
+
+# The API server won’t start up until you create the password file you specified in the
+#command line options. This is how to create the file:
 
 # The following command might work with newer versions of minikube
 #minikube start --extra-config apiserver.Authentication.PasswordFile.BasicAuthFile=/etc/kubernetes/passwd --extra-config=apiserver.Authorization.Mode=RBAC --extra-config=apiserver.Admission.PluginNames=NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,PodSecurityPolicy,ResourceQuota,DefaultTolerationSeconds
